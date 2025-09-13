5d95127bbf6d1945bdc0a55f93fe0d3e897a3d0f
Amlan Routray <amlanroutray88@gmail.com>
2025-09-13 23:02:01 +0530

    Added Footer Changes

--- src/common/TopPicks/index.jsx
+++ src/common/TopPicks/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Dialog } from "../Dialog";
 
-export const TopPicks = ({imgUrl, badgeTitle}) => {
+export const TopPicks = ({ imgUrl, badgeTitle }) => {
+  const [isDialogOpen, setDialogOpen] = React.useState(false);
+    const addOrder = () => {
+      setDialogOpen(true);
+    };
   return (
-    <section className="toppicks">
+    <section className="toppicks" onClick={addOrder}>
       <img
         src={imgUrl}
         alt="top picks item"
@@ -11,9 +16,11 @@ export const TopPicks = ({imgUrl, badgeTitle}) => {
           e.target.src = "placeholder.png";
         }}
       />
-      <p>
-        {badgeTitle}
-      </p>
+      <p>{badgeTitle}</p>
+      <Dialog isOpen={isDialogOpen} onClose={() => setDialogOpen(false)}>
+        <h2 style={{ color: "green" }}>Order Successful</h2>
+        <p>{badgeTitle} has been booked for your table</p>
+      </Dialog>
     </section>
   );
 };
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <section className="footer">
+      <div>
+        <img src="lemon.png" alt="footer" />
+        <h2>Little Lemon</h2>
+      </div>
+      <p>
+        We are passionate about serving fresh, delicious meals made with love
+        and the finest ingredients. We are your go-to spot for unforgettable
+        dining experiences, whether it’s a cozy night out or a festive
+        celebration.
+      </p>
+      <div className="footer-links-container">
+        <div>
+          <a href="/">Home</a>
+          <a href="/reservations">Reservations</a>
+          <a href="/menu">Menu</a>
+        </div>
+        <p>Master Canteen, Bhubaneswar, India, +91 0000000000</p>
+        <div>
+          <a href="/">Home</a>
+          <a href="/reservations">Reservations</a>
+          <a href="/menu">Menu</a>
+        </div>
+      </div>
+    </section>
+  );
+};
